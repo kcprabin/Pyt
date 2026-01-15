a002574ddc976bfd4fa64ca89f8097a8acff8ffb
Prabin K.C. <kcprabin2063@gmail.com>
2026-01-16 00:01:53 +0545

    uoo

--- basic.py
+++ basic.py
@@ -2,3 +2,6 @@ def lode (a,b):
     return a + b
 
 print(lode(3,4))
+
+def take(a):
+    return a
--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
 from basic import lode
+from basic import take
 import math
 
 
@@ -9,3 +10,5 @@ def calculate_area(radius):    return math.pi * radius ** 2
 
 data  = calculate_area(29)
 print(data)
+
+print(take(99))
